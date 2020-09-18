2041cef93f11cfd445466a2eff2ec2f8821a0c71
jzh <jiangzenghui005@163.com>
2020-09-18 09:23:43 +0800

    Initial Commit

---
+++ TestPasteboard/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  TestPasteboard
+//
+//  Created by jzh on 2020/9/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ TestPasteboard/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TestPasteboard
+//
+//  Created by jzh on 2020/9/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
